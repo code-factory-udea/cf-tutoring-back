b2eb0b48a5a041f027c314dfcc4487e3bcc537bf
Juan Fernando Lopera Muñoz <100541364+JL005@users.noreply.github.com>
2024-08-12 18:00:51 -0500

    fix: add swagger info to get all subjects endpoint

--- src/main/java/co/udea/codefact/subject/SubjectController.java
+++ src/main/java/co/udea/codefact/subject/SubjectController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.udea.codefact.utils.constants.EndpointConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Materias", description = "Endpoints para info sobre materias puede ser accedidos por usuarios logueados")
 @RestController
 public class SubjectController {
     
@@ -17,10 +22,11 @@ public class SubjectController {
         this.subjectService = subjectService;
     }
 
+    @Operation(summary = "Obtener materias", description = "Obtener todas las materias")
+    @ApiResponse(responseCode = "200", description = "Se obtuvieron las materias")
     @GetMapping(EndpointConstants.SUBJECT)
     public ResponseEntity<List<SubjectListDTO>> getSubjects() {
         return new ResponseEntity<>(this.subjectService.getSubjects(), HttpStatus.OK);
     }
-    
 
 }
